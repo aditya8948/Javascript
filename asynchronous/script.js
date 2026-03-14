f3b8834c3a08a634c757bd2870a06a28e4ba7bb4
aditya <89adityapandey@gmail.com>
2026-03-15 03:13:38 +0530

    asynchronous operation

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -65,7 +65,7 @@ console.log("end");
 // JSON DATA FORMATE => JavaScript Object Notation
 // most popular data format for sending and receiving data from a server
 
-
+/*
 //
 // selecting element from DOM
 const btn  = document.querySelector('.btn-country');
@@ -117,3 +117,189 @@ request.addEventListener('load' , function (){
 
 getCountryData('portugal');
 getCountryData('usa');
+
+*/
+
+const countriesContainer = document.querySelector('.countries');
+
+// function to display country data on UI
+const renderCountry = function(data){
+
+  const html = `
+          <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)} </p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"> <span>💰</span>${data.currencies[0].name}</p>
+          </div>
+          </article>
+    `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+
+
+// // function to get country and its neighbour
+// const getCountryAndNeighbour = function(country){
+
+//   // create new XMLHttpRequest object
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   // when response is loaded
+//   request.addEventListener('load', function(){
+//   const [data] = JSON.parse(this.responseText);
+//   console.log(data);
+
+//   // render first country
+//   renderCountry(data);
+
+//     // get first neighbouring country code
+//   const neighbour = data.borders?.[0];
+
+//     // if no neighbour exists stop execution
+//   if(!neighbour) return;
+
+//     // second request to get neighbour country
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
+
+//     // when neighbour data loads
+//     request2.addEventListener('load', function(){
+//     const data2 = JSON.parse(this.responseText);
+//     console.log(data2);
+
+//     // render neighbour country
+//     renderCountry(data2);
+//   });
+
+// });
+
+// };
+
+
+// // start program by calling function
+// getCountryAndNeighbour('portugal');
+
+
+
+
+// ---------------------------------------
+// Example of CALLBACK HELL
+// ---------------------------------------
+
+// setTimeout(() => {
+//   console.log("1 second passed");
+
+//   setTimeout(() => {
+//     console.log("2 seconds passed");
+
+//     setTimeout(() => {
+//       console.log("3 seconds passed");
+
+//       setTimeout(() => {
+//         console.log("4 seconds passed");
+
+//       },1000);
+
+//     },1000);
+
+//   },1000);
+
+// },1000);
+
+
+/*
+Callback Hell explanation:
+
+Multiple callbacks are nested inside each other.
+
+Structure becomes pyramid / triangle:
+
+setTimeout
+   └── setTimeout
+         └── setTimeout
+               └── setTimeout
+
+Problems:
+1. Hard to read
+2. Hard to debug
+3. Hard to maintain
+
+Solution:
+Promises
+Async / Await
+*/
+
+////////////////////////////////////////////////////////////////////////////
+
+// OLD WAY TO AJAX CALL 
+//  // create new XMLHttpRequest object
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+
+// fetch method 
+
+const request = fetch(`https://restcountries.com/v2/name/${'portugal'}`);
+console.log(request); // o.p  => Promise {<pending>}
+
+//! promise => an object that is  used as placeholder for the future result of an asynchronous operation 
+// a container for an asynchronous delivered value 
+// we no longer rely on event and callback passed into asynchronous function to handle asynchronous result ,
+// instead of nesting call back , we can chain promises for  a sequence of asynchronous operation 
+
+// ! PROMISE LIFECYCLE 
+
+// 1:  PENDING => before the value is available
+// 2:  SETTLED => asynchronous task has finished 
+// 3: FULFILLED AND REJECTED => fulfilled =>  success the  value is now available
+      // rejected => an error happened 
+
+// CONSUME PROMISE => when we already havve a  promise ex- promise returned from fetch API
+
+// DIFFERENCE BTW AJAX AND FETCH 
+// AJAX (XMLHttpRequest) IS OLDAR WAY OF MAKING ASYNCHRONOUS HTTP REQUEST 
+// FETCH API is modern way and it  return promises , making the code cleaner and easier to manage 
+
+// FUNCTION TO GET COUNTRY DATA FROM API 
+// const getCountryData = function(country){
+//   fetch(`https://restcountries.com/v2/name/${'portugal'}`) // FETCH MAKE AN HTTP REQUEST AND RETURN A PROMISE  
+//   .then(function (response){ //FIRST => .then() RUNS WHEN THE REQUEST IS  SUCCEESFUL , RESPONSE IS  THE  RAW RESPONSE OBJECT FROM THE SERVER 
+//     console.log(response);
+//     return response.json(); // CONVERT RESPONSE  BODY TO  JSON , json() ALOS RETURN PROMISE 
+//   })
+//   .then(function (data){ // SECOND => .then() RUNS WHEN JSON CONVERSION FINNISHED ,
+//     console.log(data);      // DATA IS NOW JS OBJECT 
+//     renderCountry(data[0]);
+//   })
+// }
+
+// getCountryData('portugal');
+
+// clean way 
+
+const getCountryData = function(country){
+  fetch(`https://restcountries.com/v2/name/${country}`)
+  .then(response => response.json())
+  .then(data => renderCountry(data[0]));
+}
+getCountryData('portugal');
+
+// FLOW OF EXECUTION
+// fetch() → returns Promise
+//        ↓
+// .then(response)
+//        ↓
+// response.json() → returns Promise
+//        ↓
+// .then(data)
+//        ↓
+// renderCountry(data[0])
